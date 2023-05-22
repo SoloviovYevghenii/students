213cedffc5e7357328152649815e6357892e6ad9
Home <soloviov.yevghenii@gmail.com>
2023-05-22 19:43:05 +0300

    second

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,7 @@
-import first.SameName;
-import first.SameName2;
-
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        SameName sameName = new SameName();
-        SameName2 sameName2 = new SameName2();
-
-        second.SameName sameNameSecond = new second.SameName();
-
-        Car car = new Car();
-        car.start();
-
-        Employees employees = new Employees();
-        Employees employees1 = new Employees("position","email",5,4);
-        Employees employees2 = new Employees(4524);
 
     }
 
